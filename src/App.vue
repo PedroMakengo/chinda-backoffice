c5b91c7fa88d399286298c9db2ba745f6c29a5e1
PedroMakengo <pedromakengomiguel@gmail.com>
2024-08-23 11:16:27 +0100

    Adicionado a estrutura de rotas

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <v-app>
     <v-main>
+      <nav>
+        <RouterLink to="/">Go to Home</RouterLink>
+        <RouterLink to="/about">Go to About</RouterLink>
+      </nav>
+
       <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+//
 </script>
